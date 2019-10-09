c2511203bfd6bb3722c65ac243f448e1cdcb5c96
michael <michael.malevannyy@gmail.com>
2019-10-10 01:32:23 +0300

    ++vaadin 14 sample init

---
+++ vaadin-hello-world/src/main/java/com/example/vaadinhelloworld/VaadinHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaadinhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinHelloWorldApplication.class, args);
+    }
+
+}
